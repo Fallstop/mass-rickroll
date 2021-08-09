ccfab571e58fdc8f58c1d9f05be3f017d7c029cb
Nathan Hare <me@laspruca.nz>
2021-08-09 16:21:35 +1200

    Add ability to get all online clients

--- app.js
+++ app.js
@@ -1,35 +1,41 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var expressWs = require('express-ws')(app);
-var wss = expressWs.getWss()
+var expressWs = require("express-ws")(app);
+var wss = expressWs.getWss();
 
 var clientsList = {};
 
-app.get('/', (req, res) => {
-  res.redirect("https://www.youtube.com/watch?v=o-YBDTqX_ZU&ab_channel=MusRest");
-})
+app.get("/", (req, res) => {
+  res.redirect(
+    "https://www.youtube.com/watch?v=o-YBDTqX_ZU&ab_channel=MusRest"
+  );
+});
 
-app.get('/script.ps1', (req, res) => {
-  res.sendFile('Script.ps1', {root : __dirname})
-})
+app.get("/script.ps1", (req, res) => {
+  res.sendFile("Script.ps1", { root: __dirname });
+});
 
-app.get('/rick', (req, res) => {
+app.get("/rick", (req, res) => {
   wss.clients.forEach(function each(client) {
     if (client.readyState === 1) {
       client.send("rick");
     }
   });
-  res.send('Incoming Rick Roll')
-})
-
-app.ws('/', function(ws, req) {
-  ws.on('message', function(msg) {
-    clientsList[msg] = new Date().toISOString();
-    console.log('clients list', clientsList)
-  });
-  console.log('socket', req.testing);
+  res.send("Incoming Rick Roll");
 });
 
-console.log("Server Live at http://localhost:3000")
+app.get("/active", (req, res) => {
+  res.write(wss.clients.size);
+});
+
+app.ws("/", function (ws, req) {
+  ws.on("message", function (msg) {
+    clientsList[msg] = new Date().toISOString();
+    console.log("clients list", clientsList);
+  });
+  console.log("socket", req.testing);
+});
+
+console.log("Server Live at http://localhost:3000");
 
 app.listen(3000);
